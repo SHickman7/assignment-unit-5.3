b8495675735133e5050063f11496322b6f85153a
Stephanie Hickman <stephick@gmail.com>
2024-01-11 13:23:58 -0600

    completed automated test 2:   pushes record object into the array & returns the object

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,14 +1,21 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
+//myCollection array initialized as an empty array
 let myCollection = [ ];
 
 
+// addToCollection function pushes record object into the array & returns the object
 
+function addToCollection (collection, title, artist, yearPublished ){
+  console.log ('in addToCollection', collection, title, artist, yearPublished);
+  let newAlbum = { title, artist, yearPublished }
+  collection.push(newAlbum);
+  return newAlbum;
+}
 
-
-
-
+addToCollection (myCollection, 'Red', 'Taylor Swift', 2012);
+console.log ('myCollection with Taylor Swift album: ', myCollection)
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
 // as a lil' chunk of friendly code that you don't need to understand right now.
